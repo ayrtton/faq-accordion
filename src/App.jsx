d40e792d5212b096dc063b39dc3859ebd1ff6076
Ayrton <ayrtton.vinicius@gmail.com>
2024-05-29 11:28:18 -0300

    Create the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './assets/css/main.css';
 import Background from './components/Background';
 import Card from './components/Card';
+import Footer from './components/Footer';
 
 function App() {
 	return (
 		<>
 			<Background />
 			<Card />
+			<Footer />
 		</>
 	);
 }
